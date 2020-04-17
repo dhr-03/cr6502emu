64630f512546fc9013ba0f2d9c902d4c874c4456
dhr <dhr.git.pub@gmail.com>
2020-04-17 11:38:17 -0300

    oops

--- scripts/build_debug.sh
+++ scripts/build_debug.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-clear ;ide
-printf "\033c" ;terminal
+clear #ide
+printf "\033c" #terminal
 wasm-pack build --target web --no-typescript --dev --out-name $(grep -oP "(?<=name = \")\w+(?=\")" Cargo.toml)_dev
