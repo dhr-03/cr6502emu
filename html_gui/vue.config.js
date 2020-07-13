6367c97150c4b6e875c112c8650a1110c283e668
dhr <dhr.git.pub@gmail.com>
2020-07-13 19:37:45 -0300

    ignore dev wasm in release

---
+++ html_gui/vue.config.js
@@ -0,0 +1,9 @@
+//https://github.com/vuejs/vue-cli/issues/2231
+module.exports = {
+    chainWebpack: config => {
+        config.plugin('copy').tap(([options]) => {
+            options[0].ignore.push('wasm/*_dev*')
+            return [options]
+        })
+    }
+}
