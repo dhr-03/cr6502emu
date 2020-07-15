5127579bb84f0d19b4d46db8409e19e0edd2f3e4
dhr <dhr.git.pub@gmail.com>
2020-07-15 16:11:22 -0300

    dont resolve symlinks

--- html_gui/vue.config.js
+++ html_gui/vue.config.js
@@ -1,6 +1,8 @@
 //https://github.com/vuejs/vue-cli/issues/2231
 module.exports = {
     chainWebpack: config => {
+        config.resolve.set('symlinks', false);
+
         config.plugin('copy').tap(([options]) => {
             options[0].ignore.push('wasm/*_dev*')
             return [options]
