5ddbbe72bef87f0aad72f9ef6d68afd898334021
dhr <dhr.git.pub@gmail.com>
2020-08-06 20:49:50 -0300

    mem monitor widget updater

--- html_gui/src/assets/js/deviceUpdaters.js
+++ html_gui/src/assets/js/deviceUpdaters.js
@@ -5,6 +5,20 @@ export const DeviceUpdaters = {
         Object.assign(widget.displayData, Object.fromEntries(pkg));
     },
 
+    async memMonitorUpdater(widget, pkg, memArrayBuilder) {
+        if (pkg.get("update")) {
+            if (widget.displayData.memArray === undefined) {
+                widget.displayData.memArray = memArrayBuilder();
+            } else {
+                // Vue doesnt support typed arrays for reactivity, so we need to force it.
+                let oldValue = widget.displayData.memArray;
+                widget.displayData.memArray = []
+                widget.displayData.memArray = oldValue;
+
+            }
+        }
+    },
+
     unimplemented() {
         console.warn("Unimplemented device widget updater.")
     }
