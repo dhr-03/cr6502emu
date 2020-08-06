fc43d733b97974086984579c767c41c9473101f4
dhr <dhr.git.pub@gmail.com>
2020-08-06 17:33:05 -0300

    dev utils add bool to map

--- system/src/dev/utils.rs
+++ system/src/dev/utils.rs
@@ -5,3 +5,8 @@ use wasm_bindgen::JsValue;
 pub fn js_map_add_entry_f64<T: Into<f64>>(pkg: &Map, key: &str, value: T) {
     pkg.set(&JsValue::from_str(key), &JsValue::from_f64(value.into()));
 }
+
+#[inline(always)]
+pub fn js_map_add_entry_bool(pkg: &Map, key: &str, value: bool) {
+    pkg.set(&JsValue::from_str(key), &JsValue::from_bool(value));
+}
