3eb024418f30b17071b445944f111aa926bfa485
dhr <dhr.git.pub@gmail.com>
2020-09-01 19:51:04 -0300

    screen component

---
+++ html_gui/src/components/EnvironmentWidgetPixelScreen.vue
@@ -0,0 +1,24 @@
+<template>
+    <div ref="canvasContainer"></div>
+</template>
+
+<script>
+    import MixinEnvironmentWidget from "./MixinEnvironmentWidget";
+
+    export default {
+        name: "EnvironmentWidgetPixelScreen",
+        mixins: [MixinEnvironmentWidget],
+
+        mounted() {
+          this.$refs.canvasContainer.appendChild(this.widget.displayData.canvas);
+        }
+    }
+</script>
+
+<style lang="less">
+    canvas {
+        padding: 0.5em;
+        width: 21em;
+        height: 12.5em;
+    }
+</style>
