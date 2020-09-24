9850145a824dcf2652aa42aa956725f422d83c9b
dhr <dhr.git.pub@gmail.com>
2020-09-24 19:55:23 -0300

    schema

---
+++ html_gui/src/assets/schema/project.js
@@ -0,0 +1,114 @@
+import {DeviceId} from "../wasm/system";
+
+const U16_MAX = (2 ** 16) - 1; // unsigned int 16
+
+export const ProjectSchema = {
+    type: "object",
+    properties: {
+        meta: {
+            type: "object",
+
+            default: {},
+
+            properties: {
+                name: {
+                    type: "string",
+                    default: "Unnamed Project",
+                },
+
+                code: {
+                    type: ["string", "null"],
+                    default: null,
+                },
+
+                created: {
+                    type: "integer",
+                    minimum: 0,
+                },
+
+                lastMod: {
+                    type: "integer",
+                    minimum: 0,
+                },
+
+                pid: {
+                    type: "string",
+                },
+            },
+
+            required: [
+                "name",
+                "code",
+                "created",
+                "lastMod",
+                "pid",
+            ],
+        },
+
+        settings: {
+            type: "object",
+
+            default: {},
+
+            properties: {},
+
+            required: [],
+        },
+
+        devices: {
+            type: "array",
+
+            default: [],
+
+            items: {
+                type: "object",
+
+                properties: {
+                    type: {
+                        type: "integer",
+                        enum: Object.entries(DeviceId)
+                            .map(v => v[1])
+                            .filter(v => typeof v === "number" && v !== 255),
+                    },
+
+                    uid: {
+                        type: "integer",
+                        minimum: 0,
+                        maximum: U16_MAX,
+                    },
+
+
+                    start: {
+                        type: "integer",
+                        minimum: 0,
+                        maximum: U16_MAX,
+                    },
+
+                    size: {
+                        type: "integer",
+                        minimum: 0,
+                        maximum: U16_MAX,
+                    },
+
+                    config: {
+                        type: "object",
+                    },
+                },
+
+                required: [
+                    "type",
+                    "uid",
+                    "start",
+                    "size",
+                    "config",
+                ]
+            }
+        }
+    },
+
+    required: [
+        "meta",
+        "settings",
+        "devices",
+    ],
+}
