9ee7fcc943fb32c23bffb4d8998adf45e92dea75
dhr <dhr.git.pub@gmail.com>
2020-09-25 17:54:04 -0300

    setting html

---
+++ html_gui/src/components/EnvironmentSettingPrjFile.vue
@@ -0,0 +1,45 @@
+<template>
+    <form class="uk-form-stacked">
+
+        <div class="uk-margin">
+            <label class="uk-form-label">Download Project</label>
+            <button class="crl-button cr-info uk-button">
+                <font-awesome-icon icon="download"/>
+                Download project
+            </button>
+        </div>
+
+        <hr>
+
+        <div class="uk-margin">
+            <label class="uk-form-label">Delete Project</label>
+            <button class="crl-button cr-err uk-button">
+                <font-awesome-icon icon="trash-alt"/>
+                Delete Project
+            </button>
+        </div>
+
+    </form>
+</template>
+
+<script>
+    import MixinSettingsPage from "./MixinSettingsPage";
+
+    export default {
+        name: "EnvironmentSettingPrjFile",
+
+        niceName: "File",
+
+        mixins: [MixinSettingsPage],
+
+        computed: {}
+    }
+</script>
+
+<style lang="less" scoped>
+    @import "../assets/less/modifierStyles";
+
+    .crl-button {
+        width: 15em; //same width for all
+    }
+</style>
