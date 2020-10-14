1a0208126c2bfb5be095224fdf15c7dfd2eb8d72
dhr <dhr.git.pub@gmail.com>
2020-10-14 18:46:41 -0300

    helper fns

--- system/src/cpu/opcode/operations_actions/internal.rs
+++ system/src/cpu/opcode/operations_actions/internal.rs
@@ -97,6 +97,24 @@ fn alu_sub__flag_zn(inter: &mut CPUInterface, val_1: u8, val_2: u8) -> u8 {
     inter.reg.alu
 }
 
+fn stack_push(inter: &mut CPUInterface, value: u8) {
+    inter.mem.set_addr(0x0100);
+    inter.mem.set_addr_lo(inter.reg.s);
+
+    inter.reg.s -= 1;
+
+    inter.mem.set_data(value);
+    inter.mem.write_at_addr();
+}
+
+fn stack_pull(inter: &mut CPUInterface) -> u8 {
+    inter.mem.set_addr(0x0100);
+    inter.mem.set_addr_lo(inter.reg.s);
+
+    inter.reg.s += 1;
+
+    inter.mem.read_at_addr()
+}
 
 // ############### Operations ###############
 
