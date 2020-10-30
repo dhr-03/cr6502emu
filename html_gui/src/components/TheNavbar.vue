21fd709cc052cb54557db22a098030e6f4638aff
dhr <dhr.git.pub@gmail.com>
2020-10-30 12:27:43 -0300

    fix project navbar iten

--- html_gui/src/components/TheNavbar.vue
+++ html_gui/src/components/TheNavbar.vue
@@ -5,8 +5,11 @@
 
             <ul class="uk-navbar-nav">
                 <BaseNavbarItem target="Home" :match-exact="true"/>
-                <BaseNavbarItem :target="{name: 'Project', params: {pid: '_'}}" :clickable="false"/>
-                <BaseNavbarItem target="About"/>
+                <BaseNavbarItem
+                    :target="{name: 'Project', params: {pid: currentProjectIdOrPlaceholder}}"
+                    :clickable="projectLoaded"
+                />
+                <BaseNavbarItem target="About" :match-exact="true"/>
             </ul>
         </div>
     </nav>
@@ -14,10 +17,25 @@
 
 <script>
     import BaseNavbarItem from "./BaseNavbarItem"
+    import {mapGetters} from "vuex";
 
     export default {
         name: "TheNavbar",
         components: {BaseNavbarItem},
+
+        computed: {
+            ...mapGetters("env", [
+                "currentProjectId"
+            ]),
+
+            currentProjectIdOrPlaceholder() {
+                return this.currentProjectId || "_";
+            },
+
+            projectLoaded() {
+                return this.currentProjectId != null;
+            }
+        }
     }
 </script>
 
