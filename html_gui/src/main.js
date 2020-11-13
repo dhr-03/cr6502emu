495d361cd153d12b2fd60c31832beaf8d9da3ca1
dhr <dhr.git.pub@gmail.com>
2020-11-13 11:43:52 -0300

    add new dep

--- html_gui/src/main.js
+++ html_gui/src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue"
 import App from "./App.vue"
 import router from "./router"
 import store from "./store"
+import {default as i18n} from "./i18n"
 
 import "../node_modules/uikit/dist/js/uikit.min"
 
@@ -72,6 +73,7 @@ Vue.config.productionTip = false;
 new Vue({
     router,
     store,
+    i18n,
     render: h => h(App)
 }).$mount("#app");
 
